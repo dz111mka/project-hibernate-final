8c178b797a9e2b86247792047092e6bf102bbd61
Dmitry Chepikov <chepikov.d.u@gmail.com>
2023-11-19 16:14:10 +0300

    final commit

---
+++ src/main/java/org/example/redis/Language.java
@@ -0,0 +1,14 @@
+package org.example.redis;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+
+}
